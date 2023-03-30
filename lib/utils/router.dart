08cc5a2de129768a97079e6b5db2b91d24955737
KUDOX8 <emadalialomran@petalmail.com>
2023-03-30 05:12:55 +0300

    a

--- lib/utils/router.dart
+++ lib/utils/router.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'package:myrewards_flutter/ui/pages/home_page/home_page.dart';
 import 'package:myrewards_flutter/ui/pages/profile_page/profile_page.dart';
-import 'package:myrewards_flutter/ui/pages/store_page/store_page.dart';
 import 'package:myrewards_flutter/ui/shared/pages/auth_checker/auth_checker.dart';
 
 import '../ui/pages/sign_in_page/otp_page.dart';
@@ -10,7 +9,7 @@ import '../ui/pages/sign_in_page/otp_page.dart';
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
     case '/':
-      return MaterialPageRoute(builder: (context) => const StorePage());
+      return MaterialPageRoute(builder: (context) => const HomePage());
 
     case '/home':
       return MaterialPageRoute(builder: (context) => const HomePage());
