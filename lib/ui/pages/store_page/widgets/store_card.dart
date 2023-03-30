fdc731f9b00fc0a23888bdd556be20221eab821c
KUDOX8 <emadalialomran@petalmail.com>
2023-03-30 05:15:31 +0300

    update

--- lib/ui/pages/store_page/widgets/store_card.dart
+++ lib/ui/pages/store_page/widgets/store_card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../utils/constants.dart';
