170bf8de7230808177f5ca45e163d7ee2d3c0d47
KUDOX8 <emadalialomran@petalmail.com>
2023-04-05 04:10:01 +0300

    create get post transaction feature

---
+++ lib/core/providers/transactions_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:myrewards_flutter/core/providers/user_info_provider.dart';
+import 'package:myrewards_flutter/core/services/db_services.dart';
+
+final transactionsProvider = StreamProvider((ref) {
+  final userId = ref.watch(userInfoProvider).value!.uid;
+  return DB().getTransactions(userId);
+});
