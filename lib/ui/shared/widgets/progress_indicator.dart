bb119aaa415c9f1bb4e3e7a868a6559c47aca0ba
KUDOX8 <emadalialomran@petalmail.com>
2023-04-27 23:50:43 +0300

    add package

--- lib/ui/shared/widgets/progress_indicator.dart
+++ lib/ui/shared/widgets/progress_indicator.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class ProgressIndicator extends StatelessWidget {
-  const ProgressIndicator({super.key});
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
